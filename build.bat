a98a7f9b5fd408a23a8928faf5a83a9f1299ca57
Pawel Pery <ewienik@post.pl>
2019-06-11 14:04:26 +0200

    Change build into cmake

--- build.bat
+++
@@ -1,4 +0,0 @@
-@rem @call mingwenv.bat
-@call qt484msvc2010env.bat
-@rem cl /Zi /EHsc test.cpp prefixes.cpp /link /debug /out:prefixes.exe
-cl /EHsc /O2 test.cpp prefixes.cpp /link /out:prefixes.exe
